8fd174c819c09d5f33d6c857c33b19b241b7e37f
shivamjaiswal10 <shivamjaiswal10@gmail.com>
2020-10-19 16:59:21 +0530

    YOYO

---
+++ django-ecommerce-master/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\shivam_jaiswal\desktop\ecome\django-ecommerce-master (1)\django-ecommerce-master\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
